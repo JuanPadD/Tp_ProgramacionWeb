1f623b9ed5e387262bad73644b4bdbc8ddf278e7
meli0306 <malberton382@alumno.unlam.edu.ar>
2024-06-19 19:37:52 -0300

    terminados los dos filtros del home/series/peliculas, solo falta combo temporadas - capitulos en respecto a lo dinamico. No pude terminar de arreglar una cosa con el responsive del carousel pero se ve bastante bien, no puedo hacer mas ahora, dejo por aca

--- scripts/home_dinamico.js
+++ scripts/home_dinamico.js
@@ -10,6 +10,7 @@ function agregarSeries(){
             nodo_div.classList.add('img_item')
             nodo_div.id = `${SERIES_NUEVO[i]['nombre']}`
             nodo_div.classList.add('elemento')
+            nodo_div.alt = `${SERIES_NUEVO[i]['generos']}`
             let nodo_a = document.createElement("a")
             nodo_a.href = `detalle-serie.html?indice=${i}`
             let nodo_img = document.createElement("img");
@@ -39,6 +40,7 @@ function agregarPeliculas(){
             nodo_div.classList.add('img_item')
             nodo_div.id = `${PELICULAS_NUEVO[i]['nombre']}`
             nodo_div.classList.add('elemento')
+            nodo_div.alt = `${PELICULAS_NUEVO[i]['generos']}`
             let nodo_a = document.createElement("a")
             nodo_a.href = `detalle-pelicula.html?indice=${i}`
             let nodo_img = document.createElement("img");
@@ -62,16 +64,37 @@ if(document.title != "Flimpop Series"){
 }
 
 const buscador  = document.getElementById("search-input")
-buscador.addEventListener('input', filterList)
+buscador.addEventListener('input', filtroBuscador)
 
 
-function filterList(){
+function filtroBuscador(){
     const buscador = document.getElementById("search-input")
     const filtro = buscador.value.toLowerCase()
     const listaImagenes = document.querySelectorAll(".elemento")
 
     listaImagenes.forEach((item) =>{
         let texto = item.id
+        if(texto.toLowerCase().includes(filtro.toLowerCase())){
+            item.style.display = '';
+        }
+        else{
+            item.style.display = 'none';
+        }
+    })
+
+}
+
+const selector  = document.getElementById("select_category_home")
+selector.addEventListener('change', filtroSelector)
+
+
+function filtroSelector(){
+    const selector = document.getElementById("select_category_home")
+    const filtro = selector.value.toLowerCase()
+    const listaImagenes = document.querySelectorAll(".elemento")
+
+    listaImagenes.forEach((item) =>{
+        let texto = item.alt
         if(texto.toLowerCase().includes(filtro.toLowerCase())){
             item.style.display = '';
         }
